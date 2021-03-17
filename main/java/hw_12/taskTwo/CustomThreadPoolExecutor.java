790062c33d7fc66ef693a6fafcf52adb9d334f4d
KuzmenkoO <sawkone@ukr.net>
2021-03-17 22:05:58 +0200

    Add @Target

--- main/java/hw_12/taskTwo/CustomThreadPoolExecutor.java
+++ main/java/hw_12/taskTwo/CustomThreadPoolExecutor.java
@@ -1,12 +1,14 @@
 package hw_12.taskTwo;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @interface Repeat {
     int value();
